3a7c7dd344f517405851d584336ce433d27e431a
Ebru Rice <ebruerenb@gmail.com>
2021-10-31 22:07:52 -0700

    Add js hide, show functions

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $("#fc1").click(function(){
+    $("#1fc").toggle();
+  });
+  $("#fc2").click(function(){
+    $("#2fc").toggle();
+  });
+  $("#fc3").click(function(){
+    $("#3fc").toggle();
+  });
+  $("#fc4").click(function(){
+    $("#4fc").toggle();
+  });
+  $("#fc5").click(function(){
+    $("#5fc").toggle();
+  });
+  $("#fc6").click(function(){
+    $("#6fc").toggle();
+  });
+  $("#fc7").click(function(){
+    $("#7fc").toggle();
+  });
+  $("#fc8").click(function(){
+    $("#8fc").toggle();
+  });
+});
